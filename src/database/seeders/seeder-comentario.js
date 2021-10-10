1c11792c579504c60c91e3b32f0101516b8a041d
Dragondes <cevs12321@gmail.com>
2021-10-10 18:21:40 -0400

    datosSeeder

--- src/database/seeders/seeder-comentario.js
+++ src/database/seeders/seeder-comentario.js
@@ -5,13 +5,30 @@ module.exports = {
         return queryInterface.bulkInsert('Comentarios', 
         [   
             {
-                descripcion: 'buen gusto',
+                descripcion: 'Buen gusto',
                 RestauranteId: 1
             },
             {
                 descripcion: 'Me gusto el ambiente',
-                RestauranteId: 1
+                RestauranteId: 2
+            },
+            {
+                descripcion: 'Muy buen servicio',
+                RestauranteId: 4
+            },
+            {
+                descripcion: 'Me gusta la textura de la comida',
+                RestauranteId: 4
+            },
+            {
+                descripcion: 'No crei que exista una saltenia peor que las del frente del ic norte',
+                RestauranteId: 7            
+            },
+            {
+                descripcion: 'Me gusto el ambiente',
+                RestauranteId: 3
             }
+
         ]);
     },
 
