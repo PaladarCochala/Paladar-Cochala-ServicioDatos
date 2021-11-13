7e0069ccee902cd89636680e23ed31fb90acdb19
Allen Vargas <allenbfox@gmail.com>
2021-11-12 22:09:27 -0400

    End-point para comentarios detallados por restaurante

--- src/routes/comentarios.js
+++ src/routes/comentarios.js
@@ -18,5 +18,6 @@ router.put( Rutas.id, comentarioService.actualizarComentario );
 
 // api/comentaios/restaurante/:restauranteId
 router.get( '/restaurante/:restauranteId', comentarioService.obtenerComentariosPorRestaurante);
+router.get( '/detallado/restaurante/:restauranteId', comentarioService.obtenerComentariosDetalladosPorRestaurante);
 
 module.exports = router;
